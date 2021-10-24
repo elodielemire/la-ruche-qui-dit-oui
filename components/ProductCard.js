abb45ead08a7cc1e6b5f620f728b8a1b214b4617
elodielemire <elodie.lemire@isen-lille.fr>
2021-10-24 19:38:57 +0200

    fix: ne pas pouvoir aller sur les elements de la carte avec Tab

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -11,13 +11,13 @@ function ProductCard({ item, router }) {
 
     return <div className={styles.product_card} onKeyPress={(e) => handleKeypress(e, item)} tabIndex="0" aria-label={"Ouvir le produit " + item.product_name}>
         <Link href={"/product/" + item.id}>
-            <a>
+            <a tabIndex="-1">
                 {item.image_front_small_url ?
                     <div className={styles.product_card__img_container}><img src={item.image_front_small_url} alt={item.product_name} className={styles.product_card__img} /></div>
                     : <div className={styles.product_card__img_container}><img src="/picture-missing.jpg" alt="Pas d'image produit" className={styles.product_card__img} /></div>}
 
                 <p className={styles.product_card__text}>{item.product_name ? item.product_name : 'Nom du produit manquant'}</p>
-                <button className={styles.product_card__button}>Voir plus</button>
+                <button tabIndex="-1" aria-label={"Voir plus sur" + item.product_name} className={styles.product_card__button}>Voir plus</button>
             </a>
         </Link>
     </div>
