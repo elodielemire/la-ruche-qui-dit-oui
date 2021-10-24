27df4879ce5ebab3a8959f4821e85cbd6aaaec02
elodielemire <elodie.lemire@isen-lille.fr>
2021-10-24 15:10:28 +0200

    fix: l'image du produit n'est plus etiree

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -5,8 +5,8 @@ function ProductCard({ item }) {
     return <Link href={"/product/" + item.id}>
         <a>
             {item.image_front_small_url ?
-                <img src={item.image_front_small_url} alt={item.product_name} className={styles.product_card__img} />
-                : <img src="/picture-missing.jpg" alt="Pas d'image produit" className={styles.product_card__img} />}
+                <div className={styles.product_card__img_container}><img src={item.image_front_small_url} alt={item.product_name} className={styles.product_card__img} /></div>
+                : <div className={styles.product_card__img_container}><img src="/picture-missing.jpg" alt="Pas d'image produit" className={styles.product_card__img} /></div>}
 
             <p className={styles.product_card__text}>{item.product_name ? item.product_name : 'Nom du produit manquant'}</p>
             <button className={styles.product_card__button}>Voir plus</button>
